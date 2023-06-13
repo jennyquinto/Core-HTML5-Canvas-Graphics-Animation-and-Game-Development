5663cd5e4608d9011935492e88817cf01c1972f8
jennyquinto <vane.quinto19@gmail.com>
2023-06-12 21:31:55 -0500

    Fundamental drawing operations

--- ch01/Example-1.1/main.js
+++
@@ -1,9 +0,0 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-
-context.font = '38pt Arial';
-context.fillStyle = 'cornflowerblue';
-context.strokeStyle = 'black';
-
-context.fillText('Hello, world!', canvas.width / 2 - 150, canvas.height / 2 + 15);
-context.strokeText('Hello, world!', canvas.width / 2 - 150, canvas.height / 2 + 15);
